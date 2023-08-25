4c1a3dfdfe3a29ab5e0af68cf8139e756bee2029
OliBomby <olivierschipper@gmail.com>
2023-08-25 22:21:00 +0200

    fix attention

--- models.py
+++ models.py
@@ -138,7 +138,7 @@ class DiTBlock(nn.Module):
     def forward(self, x, c):
         shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = self.adaLN_modulation(c).chunk(6, dim=1)
         modulated = modulate(self.norm1(x), shift_msa, scale_msa)
-        x = x + gate_msa.unsqueeze(1) * self.attn(modulated, modulated, modulated)
+        x = x + gate_msa.unsqueeze(1) * self.attn(modulated, modulated, modulated, need_weights=False)[0]
         x = x + gate_mlp.unsqueeze(1) * self.mlp(modulate(self.norm2(x), shift_mlp, scale_mlp))
         return x
 
