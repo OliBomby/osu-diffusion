16566ff51f462b5e7dbc83dd7813e9e403a4c44e
NiceAesth <nice@aesth.dev>
2023-09-04 20:06:58 +0300

    chore: fix torch run

--- train.sh
+++ train.sh
@@ -18,4 +18,4 @@
 module load cuda
 module load python/3.11.4
 # Run the executable
-torchrun --nproc-per-node=4 python train.py --data-path "../ORS13402_no_audio" --model DiT-XL --num-workers 12 --epochs 100 --global-batch-size 256 --ckpt-every 20000 --seq-len 128
+torchrun --nproc-per-node=4 train.py --data-path "../ORS13402_no_audio" --model DiT-XL --num-workers 12 --epochs 100 --global-batch-size 256 --ckpt-every 20000 --seq-len 128
