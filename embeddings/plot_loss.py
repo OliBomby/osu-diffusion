729c648ed7a0871d32c046c70ce93230c2b9f48f
OliBomby <olivierschipper@gmail.com>
2023-09-16 20:31:11 +0200

    update loss plot

--- embeddings/plot_loss.py
+++ embeddings/plot_loss.py
@@ -11,6 +11,7 @@ log_files = [
     ("D:\\Osu! Dingen\\Beatmap ML Datasets\\results\\new2\\r\\log.txt", 1600),
     ("D:\\Osu! Dingen\\Beatmap ML Datasets\\results\\new2\\r\\clean\\log.txt", 4000),
     ("D:\\Osu! Dingen\\Beatmap ML Datasets\\results\\new2\\r\\cosine\\log.txt", 4000),
+    ("D:\\Osu! Dingen\\Beatmap ML Datasets\\results\\new2\\r\\cosine\\l1\\log.txt", 4600),
 ]
 
 for path, x_offset in log_files:
