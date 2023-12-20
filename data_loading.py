b5a96a39ccb9e644f92dab1d1661269f406f2457
OliBomby <olivierschipper@gmail.com>
2023-12-20 18:59:25 +0100

    update data loading

--- data_loading.py
+++ data_loading.py
@@ -1,16 +1,16 @@
-import json
 import math
 import os.path
 import pickle
 import random
 from collections.abc import Callable
 from datetime import timedelta
-from pathlib import Path
+from pathlib import Path, PurePosixPath, PureWindowsPath
 from typing import Optional
 
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torch.utils.data import IterableDataset
+import tqdm
 
 from positional_embedding import offset_sequence_embedding
 from positional_embedding import position_sequence_embedding
@@ -257,22 +257,15 @@ class InterleavingBeatmapDatasetIterable:
     def __init__(
         self,
         beatmap_files: list[str],
-        seq_len: int,
-        stride: int,
+        iterable_factory: Callable,
         cycle_length: int,
-        seq_func: Optional[Callable] = None,
-        win_func: Optional[Callable] = None,
     ):
         per_worker = int(math.ceil(len(beatmap_files) / float(cycle_length)))
         self.workers = [
-            BeatmapDatasetIterable(
+            iterable_factory(
                 beatmap_files[
-                    i * per_worker : min(len(beatmap_files), (i + 1) * per_worker)
-                ],
-                seq_len,
-                stride,
-                seq_func,
-                win_func,
+                    i * per_worker: min(len(beatmap_files), (i + 1) * per_worker)
+                ]
             )
             for i in range(cycle_length)
         ]
@@ -301,62 +294,39 @@ class BeatmapDataset(IterableDataset):
         dataset_path: str,
         start: int,
         end: int,
-        seq_len: int,
-        stride: int = 1,
+        iterable_factory: Callable,
         cycle_length: int = 1,
         shuffle: bool = False,
-        subset_ids: list[int] | None = None,
-        seq_func: Optional[Callable] = None,
-        win_func: Optional[Callable] = None,
+        beatmap_files: Optional[list[str]] = None,
     ):
         super(BeatmapDataset).__init__()
         self.dataset_path = dataset_path
         self.start = start
         self.end = end
-        self.seq_len = seq_len
-        self.stride = stride
+        self.iterable_factory = iterable_factory
         self.cycle_length = cycle_length
         self.shuffle = shuffle
-        self.subset_ids = subset_ids
-        self.seq_func = seq_func
-        self.win_func = win_func
+        self.beatmap_files = beatmap_files
 
     def _get_beatmap_files(self) -> list[str]:
+        if self.beatmap_files is not None:
+            return self.beatmap_files
+
         # Get a list of all beatmap files in the dataset path in the track index range between start and end
         beatmap_files = []
         track_names = ["Track" + str(i).zfill(5) for i in range(self.start, self.end)]
         for track_name in track_names:
-            if self.subset_ids is not None:
-                metadata_File = os.path.join(
-                    self.dataset_path,
-                    track_name,
-                    "metadata.json",
-                )
-                with open(metadata_File) as f:
-                    metadata = json.load(f)
-                for beatmap_name in metadata["Beatmaps"]:
-                    beatmap_metadata = metadata["Beatmaps"][beatmap_name]
-                    if beatmap_metadata["BeatmapId"] in self.subset_ids:
-                        beatmap_files.append(
-                            os.path.join(
-                                self.dataset_path,
-                                track_name,
-                                "beatmaps",
-                                beatmap_name + ".osu",
-                            ),
-                        )
-            else:
-                for beatmap_file in os.listdir(
+            for beatmap_file in os.listdir(
                     os.path.join(self.dataset_path, track_name, "beatmaps"),
-                ):
-                    beatmap_files.append(
-                        os.path.join(
-                            self.dataset_path,
-                            track_name,
-                            "beatmaps",
-                            beatmap_file,
-                        ),
-                    )
+            ):
+                beatmap_files.append(
+                    os.path.join(
+                        self.dataset_path,
+                        track_name,
+                        "beatmaps",
+                        beatmap_file,
+                    ),
+                )
 
         return beatmap_files
 
@@ -369,20 +339,11 @@ class BeatmapDataset(IterableDataset):
         if self.cycle_length > 1:
             return InterleavingBeatmapDatasetIterable(
                 beatmap_files,
-                self.seq_len,
-                self.stride,
+                self.iterable_factory,
                 self.cycle_length,
-                self.seq_func,
-                self.win_func,
             )
 
-        return BeatmapDatasetIterable(
-            beatmap_files,
-            self.seq_len,
-            self.stride,
-            self.seq_func,
-            self.win_func,
-        )
+        return self.iterable_factory(beatmap_files)
 
 
 # Define a `worker_init_fn` that configures each dataset copy differently
@@ -406,34 +367,121 @@ def get_beatmap_idx(name) -> dict[int, int]:
     return beatmap_idx
 
 
-def get_processed_data_loader(
-    dataset_path: str,
-    start: int,
-    end: int,
-    seq_len: int,
-    stride: int = 1,
-    cycle_length=1,
-    batch_size: int = 1,
-    num_workers: int = 0,
-    shuffle: bool = False,
-    pin_memory: bool = False,
-    drop_last: bool = False,
-    subset_ids: list[int] | None = None,
-    seq_func: Optional[Callable] = None,
-    win_func: Optional[Callable] = None,
+def get_beatmap_files(name: str, data_path: str) -> list[PurePosixPath]:
+    p = Path(name)
+    with p.open("rb") as f:
+        relative_beatmap_files = pickle.load(f)
+    beatmap_files = [PurePosixPath(data_path, *PureWindowsPath(f).parts) for f in relative_beatmap_files]
+    return beatmap_files
+
+
+class BeatmapDatasetIterableFactory:
+    __slots__ = ("seq_len", "stride", "seq_func", "win_func")
+
+    def __init__(self, seq_len, stride, seq_func, win_func):
+        self.seq_len = seq_len
+        self.stride = stride
+        self.seq_func = seq_func
+        self.win_func = win_func
+
+    def __call__(self, *args, **kwargs):
+        beatmap_files = args[0]
+        return BeatmapDatasetIterable(
+            beatmap_files=beatmap_files,
+            seq_len=self.seq_len,
+            stride=self.stride,
+            seq_func=self.seq_func,
+            win_func=self.win_func,
+        )
+
+
+class CachedDataset(Dataset):
+    __slots__ = "cached_data"
+
+    def __init__(self, cached_data):
+        self.cached_data = cached_data
+
+    def __getitem__(self, index):
+        return self.cached_data[index]
+
+    def __len__(self):
+        return len(self.cached_data)
+
+
+def cache_dataset(
+        out_path: str,
+        dataset_path: str,
+        start: int,
+        end: int,
+        iterable_factory: Callable,
+        cycle_length=1,
+        beatmap_files: Optional[list[str]] = None,
+):
+    dataset = BeatmapDataset(
+        dataset_path=dataset_path,
+        start=start,
+        end=end,
+        iterable_factory=iterable_factory,
+        cycle_length=cycle_length,
+        shuffle=False,
+        beatmap_files=beatmap_files,
+    )
+
+    print("Caching dataset...")
+    cached_data = []
+    for datum in tqdm.tqdm(dataset):
+        cached_data.append(datum)
+
+    torch.save(cached_data, out_path)
+
+
+def get_cached_data_loader(
+        data_path: str,
+        batch_size: int = 1,
+        num_workers: int = 0,
+        shuffle: bool = False,
+        pin_memory: bool = False,
+        drop_last: bool = False,
+):
+    cached_data = torch.load(data_path)
+    dataset = CachedDataset(cached_data)
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        drop_last=drop_last,
+        persistent_workers=num_workers > 0,
+        shuffle=shuffle,
+    )
+
+    return dataloader
+
+
+def get_data_loader(
+        dataset_path: str,
+        start: int,
+        end: int,
+        iterable_factory: Callable,
+        cycle_length=1,
+        batch_size: int = 1,
+        num_workers: int = 0,
+        shuffle: bool = False,
+        pin_memory: bool = False,
+        drop_last: bool = False,
+        beatmap_files: Optional[list[str]] = None,
 ) -> DataLoader:
     dataset = BeatmapDataset(
         dataset_path=dataset_path,
         start=start,
         end=end,
-        seq_len=seq_len,
-        stride=stride,
+        iterable_factory=iterable_factory,
         cycle_length=cycle_length,
         shuffle=shuffle,
-        subset_ids=subset_ids,
-        seq_func=seq_func,
-        win_func=win_func,
+        beatmap_files=beatmap_files,
     )
+
     dataloader = DataLoader(
         dataset,
         batch_size=batch_size,
@@ -441,60 +489,64 @@ def get_processed_data_loader(
         num_workers=num_workers,
         pin_memory=pin_memory,
         drop_last=drop_last,
+        persistent_workers=num_workers > 0,
     )
 
     return dataloader
 
 
-if __name__ == "__main__":
-    # batch_size = 256
-    # num_workers = 4
-    batch_size = 1
-    num_workers = 0
-    # import pandas as pd
-    # subset_ids = pd.read_csv("C:\\Users\\Olivier\\Documents\\GitHub\\osu-diffusion\\results\\tags\\clean_10000.csv")["BeatmapID"].tolist()
-    dataloader = get_processed_data_loader(
-        dataset_path="D:\\Osu! Dingen\\Beatmap ML Datasets\\ORS16291",
+def main(args):
+    dataloader = get_data_loader(
+        dataset_path=args.data_path,
         start=0,
-        end=13402,
-        seq_len=128,
-        stride=16,
+        end=16291,
+        iterable_factory=BeatmapDatasetIterableFactory(
+            128,
+            16,
+            load_and_process_beatmap,
+            window_and_relative_time,
+        ),
         cycle_length=1,
-        batch_size=batch_size,
-        num_workers=num_workers,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
         shuffle=False,
         pin_memory=False,
         drop_last=True,
-        # subset_ids=subset_ids,
-        seq_func=load_and_process_beatmap,
-        win_func=window_and_relative_time,
     )
 
-    import matplotlib.pyplot as plt
+    if args.mode == "plotfirst":
+        import matplotlib.pyplot as plt
 
-    for (x, o, c), y in dataloader:
-        x = torch.swapaxes(x, 1, 2)  # (N, T, C)
-        c = torch.swapaxes(c, 1, 2)  # (N, T, E)
-        print(x.shape, o.shape, c.shape, y.shape)
-        batch_pos_emb = position_sequence_embedding(x * playfield_size, 128)
-        print(batch_pos_emb.shape)
-        batch_offset_emb = offset_sequence_embedding(o / 10, 128)
-        print(batch_offset_emb.shape)
-        print(y)
+        for (x, o, c), y in dataloader:
+            x = torch.swapaxes(x, 1, 2)  # (N, T, C)
+            c = torch.swapaxes(c, 1, 2)  # (N, T, E)
+            print(x.shape, o.shape, c.shape, y.shape)
+            batch_pos_emb = position_sequence_embedding(x * playfield_size, 128)
+            print(batch_pos_emb.shape)
+            batch_offset_emb = offset_sequence_embedding(o / 10, 128)
+            print(batch_offset_emb.shape)
+            print(y)
 
-        for j in range(batch_size):
-            fig, axs = plt.subplots(3, figsize=(5, 20))
-            axs[0].imshow(batch_pos_emb[j])
-            axs[1].imshow(batch_offset_emb[j])
-            axs[2].imshow(c[j])
-            print(y[j])
-            plt.show()
-        break
+            for j in range(args.batch_size):
+                fig, axs = plt.subplots(3, figsize=(5, 20))
+                axs[0].imshow(batch_pos_emb[j])
+                axs[1].imshow(batch_offset_emb[j])
+                axs[2].imshow(c[j])
+                print(y[j])
+                plt.show()
+            break
+    elif args.mode == "benchmark":
+        for _ in tqdm.tqdm(dataloader, total=7000, smoothing=0.01):
+            pass
 
-    # import time
-    # import tqdm
-    # count = 0
-    # start = time.time()
-    # for f in tqdm.tqdm(dataloader, total=76200, smoothing=0.01):
-    #     count += 1
-    #     # print(f"\r{count}, {count / (time.time() - start)} per second, beatmap index {torch.max(f[1])}", end='')
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-path", type=str, required=True)
+    parser.add_argument("--mode", type=str, required=True)
+    parser.add_argument("--batch-size", type=int, default=1)
+    parser.add_argument("--num-workers", type=int, default=0)
+    args = parser.parse_args()
+    main(args)
